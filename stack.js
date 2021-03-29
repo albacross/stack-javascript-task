9e09847d325880fe22846f747704d079244e40c9
Paweł Kocielny <pawel.koscielny2904@gmail.com>
2021-03-29 19:21:40 +0200

    FORK-0001: Add class stack

--- stack.js
+++ stack.js
@@ -1,1 +1,39 @@
 // Your implementation goes here
+module.exports = class Stack {
+  constructor(...stack) {
+    this.stack = stack;
+  }
+
+  get size() {
+    return this.stack.length;
+  }
+
+  _reArrangeStack(...stack) {
+    this.stack = stack;
+  }
+
+  push(newElement) {
+    this.stack.push(newElement);
+  }
+
+  pop() {
+    if (this.isEmpty()) return null;
+    return this.stack.pop();
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  peek() {
+    return this.stack[this.size - 1];
+  }
+
+  swap() {
+    const lastElement = this.stack[this.size - 1];
+    const secondFromTheEndElement = this.stack[this.size - 2];
+    const rest = this.stack.splice(0, this.size - 2);
+
+    this._reArrangeStack(...rest, lastElement, secondFromTheEndElement);
+  }
+};
